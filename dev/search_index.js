ed21a5ff08d2148f6bab398a3577c20c76013311
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2018-12-15 09:51:08 +0000

    build based on 9ef6eb5

---
+++ dev/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "SatelliteDynamics.jl Documentation",
+    "title": "SatelliteDynamics.jl Documentation",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#SatelliteDynamics.jl-Documentation-1",
+    "page": "SatelliteDynamics.jl Documentation",
+    "title": "SatelliteDynamics.jl Documentation",
+    "category": "section",
+    "text": ""
+},
+
+]}
