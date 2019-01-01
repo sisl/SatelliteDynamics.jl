6fd5cf7eef38ad962c0a8d92309f5eb622585f2e
Duncan Eddy <duncan.eddy@gmail.com>
2019-01-01 11:38:45 -0600

    Attempt to fix build script.

--- utils/build_setup.sh
+++ utils/build_setup.sh
@@ -5,7 +5,7 @@ if [["$TRAVIS_OS_NAME" == "linux"]]; then
     sudo apt-get update -qq
     sudo apt-get install -qq x11-apps libxt6 libxrender1 libxext6 libgl1-mesa-glx qtbase5-dev qtdeclarative5-dev
     sudo apt-get install -y --no-install-recommends texlive-fonts-recommended texlive-latex-extra texlive-fonts-extra dvipng texlive-latex-recommended
-elif  [[ "$TRAVIS_OS_NAME" == "osx" ]];
+elif  [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew update
     brew install qt5
     brew cask install basictex
