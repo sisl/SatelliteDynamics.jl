f716cdce806863dfcdfe1131dfb854d7edcdce7c
Duncan Eddy <duncan.eddy@gmail.com>
2019-01-01 11:30:42 -0600

    Attempt to fix use of bash script in CI build.

--- utils/build_setup.sh
+++ utils/build_setup.sh
@@ -5,7 +5,6 @@ if [["$TRAVIS_OS_NAME" == "linux"]]; then
     sudo apt-get update -qq
     sudo apt-get install -qq x11-apps libxt6 libxrender1 libxext6 libgl1-mesa-glx qtbase5-dev qtdeclarative5-dev
     sudo apt-get install -y --no-install-recommends texlive-fonts-recommended texlive-latex-extra texlive-fonts-extra dvipng texlive-latex-recommended
-    export GKSwstype=svg
 elif  [[ "$TRAVIS_OS_NAME" == "osx" ]];
     brew update
     brew install qt5
