4b61985323593ec9549f30e209c6cca2feeda3a2
Duncan Eddy <duncan.eddy@gmail.com>
2024-05-27 17:52:28 -0700

    Attempt to fix documentation

--- docs/make.jl
+++ docs/make.jl
@@ -19,7 +19,7 @@ makedocs(
         canonical="https://sisl.github.io/SatelliteDynamics.jl",
         edit_link="main",
         assets=String[],
-        prettyurls = get(ENV, "CI", nothing) == "true"
+        prettyurls = false # get(ENV, "CI", nothing) == "true"
     ),
     sitename  = "SatelliteDynamics.jl",
     authors   = "Duncan Eddy",
